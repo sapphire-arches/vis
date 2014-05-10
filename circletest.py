ab5befdcb99871f3a76509ab48fe32d02708d40e
bobtwinkles <srkoser+GitHub@gmail.com>
2014-05-10 14:57:31 -0400

    Add layer of abstraction over VBOs

--- circletest.py
+++ circletest.py
@@ -1,4 +1,5 @@
 from windowmanagement import create_window, main_loop
+from globjects import VertexBufferObject,VertexAttribute
 from OpenGL.GL import *
 from OpenGL.GL import shaders
 from OpenGL.arrays import vbo
@@ -14,25 +15,12 @@ def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     shaders.glUseProgram(shader)
     try:
-        vertex_buffer.bind()
-        try:
-            glEnableClientState(GL_VERTEX_ARRAY)
-            glEnableClientState(GL_COLOR_ARRAY)
-            glVertexPointer(3, GL_FLOAT, 24, vertex_buffer)
-            glColorPointer(3, GL_FLOAT, 24, vertex_buffer + 12)
-            glDrawArrays(GL_TRIANGLES, 0, len(vertex_buffer))
-        finally:
-            vertex_buffer.unbind()
-            glDisableClientState(GL_VERTEX_ARRAY)
-            glDisableClientState(GL_COLOR_ARRAY)
+      vertex_buffer.render()
     finally:
         shaders.glUseProgram(0)
 
-def make_point(pos, color):
-    return [[pos[0], pos[1], pos[2], color[0], color[1], color[2]]]
-
-def make_circle_point(angle, radius, color):
-    return make_point((cos(angle) * radius, sin(angle) * radius, 0), color)
+def make_circle_point(angle, radius):
+    return (cos(angle) * radius, sin(angle) * radius, 0)
 
 def make_circle(inner_radius, outer_radius, steps):
     delta = 2 * pi / steps
@@ -41,21 +29,21 @@ def make_circle(inner_radius, outer_radius, steps):
     saturation = 0.7
     value = 0.7
     points = []
+    colors = []
     for i in range(0, steps):
         angle = i * delta
         color1 = hsv_to_rgb(hue            , saturation, value)
         color2 = hsv_to_rgb(hue + hue_delta, saturation, value)
-        points += make_circle_point(angle        , inner_radius, color1)
-        points += make_circle_point(angle + delta, outer_radius, color2)
-        points += make_circle_point(angle        , outer_radius, color1)
-        points += make_circle_point(angle        , inner_radius, color1)
-        points += make_circle_point(angle + delta, inner_radius, color2)
-        points += make_circle_point(angle + delta, outer_radius, color2)
+        points += make_circle_point(angle        , inner_radius); colors +=  color1
+        points += make_circle_point(angle + delta, outer_radius); colors +=  color2
+        points += make_circle_point(angle        , outer_radius); colors +=  color1
+        points += make_circle_point(angle        , inner_radius); colors +=  color1
+        points += make_circle_point(angle + delta, inner_radius); colors +=  color2
+        points += make_circle_point(angle + delta, outer_radius); colors +=  color2
         if i == steps / 2:
             hue_delta = -hue_delta
         hue += hue_delta
-
-    return np.array(points, 'f')
+    return VertexBufferObject(VertexAttribute(points), colors=VertexAttribute(colors))
 
 def resize(w, h):
     glLoadIdentity()
@@ -72,10 +60,7 @@ def init():
     vert_shader = shaders.compileShader(vert_source, GL_VERTEX_SHADER)
     frag_shader = shaders.compileShader(frag_source, GL_FRAGMENT_SHADER)
     shader = shaders.compileProgram(vert_shader, frag_shader)
-    circle = make_circle(100, 200, 32)
-    print(circle)
-    arr = np.array([ [ 0, 1, 0 ], [ -1,-1, 0 ], [ 1,-1, 0 ], [ 2,-1, 0 ], [ 4,-1, 0 ], [ 4, 1, 0 ], [ 2,-1, 0 ], [ 4, 1, 0 ], [ 2, 1, 0 ], ], 'f')
-    vertex_buffer = vbo.VBO(circle)
+    vertex_buffer = make_circle(100, 200, 32)
 
 create_window("circles", display=display, resize=resize)
 init()
---
+++ globjects.py
@@ -0,0 +1,102 @@
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+from OpenGL.arrays import vbo
+from collections import Iterable, Sequence
+import numpy as np
+
+def _type_check(val, errormessage, *types):
+    for type_ in types:
+        if not isinstance(val, type_):
+            raise TypeError(errormessage % (str(val)))
+
+class VertexAttribute(Iterable):
+    def __init__(self, data, components=3):
+        _type_check(data, "Data must be iterable and a sequence but is of type %s", Iterable, Sequence)
+        self._data = data
+        self._is_sequence = isinstance(data[0], Sequence)
+        if self._is_sequence:
+            self._length = len(data)
+        else:
+            if not len(data) % components == 0:
+                raise ValueError("Number of data elements %d is not evenly divisible by number of components %d", (len(data), components))
+            self._length = len(data) // components
+            self.components = components
+
+    def __getitem__(self, i):
+        if i < 0 or i >= self._length:
+            raise IndexError("Index out of range (%d / %d)" % (i, self._length))
+        if self._is_sequence:
+            return self._data[i]
+        else:
+            return self._data[i * self.components : (i + 1) * self.components]
+
+    def __len__(self):
+        return self._length
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+class VertexBufferObject:
+    def __init__(self, positions, colors=None, mode=GL_TRIANGLES):
+        data = None
+
+        if not isinstance(positions, VertexAttribute):
+            raise TypeError("Positions must be an instance of VertexAttribute")
+
+        self._has_color = colors is not None
+
+        if colors is not None:
+            if not isinstance(colors, VertexAttribute):
+                raise TypeError("Colors must be an instance of VertexAttribute")
+
+            if not len(colors) == len(positions):
+                raise TypeError("Colors and positions must be the same length (are %d vs %d)" % (len(color), len(positions)))
+
+            data_backing = []
+            for i in range(len(positions)):
+                data_backing += positions[i]
+                data_backing += colors[i]
+            data = np.array(data_backing, 'f')
+        else:
+            data = np.array(positions._data, 'f')
+
+        np.savetxt('points.txt', data)
+        self._mode = mode
+        self._vbo = vbo.VBO(data)
+        self._length = len(colors)
+
+    def render(self):
+        self._vbo.bind()
+        data_size = 0
+        if self._has_color:
+            data_size = 24
+        else:
+            data_size = 12
+        try:
+            glEnableClientState(GL_VERTEX_ARRAY)
+            glVertexPointer(3, GL_FLOAT, data_size, self._vbo)
+            if self._has_color:
+                glEnableClientState(GL_COLOR_ARRAY)
+                glColorPointer(3, GL_FLOAT, data_size, self._vbo + 12)
+            glDrawArrays(self._mode, 0, self._length)
+        finally:
+            self._vbo.unbind()
+            glDisableClientState(GL_VERTEX_ARRAY)
+            if self._has_color:
+                glDisableClientState(GL_COLOR_ARRAY)
+
+
+class ShaderProgram:
+    def __init__(self, vert, frag):
+        vert_source = open(vert).read().encode('ascii')
+        frag_source = open(frag).read().encode('ascii')
+        vert_shader = shaders.compileShader(vert_source, GL_VERTEX_SHADER)
+        frag_shader = shaders.compileShader(frag_source, GL_FRAGMENT_SHADER)
+        self.program = shaders.compileProgram(vert_shader, frag_shader)
+
+    def bind(self):
+        shaders.glUseProgram(self.program)
+
+    def unbind(self):
+        shaders.glUseProgram(0)
