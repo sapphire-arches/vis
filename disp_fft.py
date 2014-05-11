0de3239c74fdf3ed76e9ec8d77abbcf51b680f30
bobtwinkles <srkoser+GitHub@gmail.com>
2014-05-10 23:26:48 -0400

    play in realtime

--- disp_fft.py
+++ disp_fft.py
@@ -7,52 +7,83 @@ from math import pi
 import numpy as np
 from numpy.fft import fft
 from sys import argv
+from time import time
 
 audio = AudioSegment.from_mp3(argv[1])
-seg = [x - 127.5 for x in audio._data]
-print("loaded sound, running fft")
-freqs = np.abs(fft(seg))
-freqs = freqs[0:len(freqs)//2]
-print("fft completed")
+full_audio = []
+for i in range(int(audio.frame_count())):
+    full_audio.append(int.from_bytes(audio.get_frame(i), 'little') - 32768)
 
-# bucketify things
-buckets = []
-num_buckets = 1024
-bucket_width = int(len(freqs) / num_buckets)
-for i in range(len(freqs) // bucket_width):
-    buckets.append(sum(freqs[i * bucket_width : (i + 1) * bucket_width]))
+audio_length = 1000 * audio.frame_count() / audio.frame_rate
 
-bucket_max = max(buckets)
-
-buckets = [x / bucket_max for x in buckets]
-
-verts = []
 colors = []
 
-xdelta = 800 / len(buckets)
-huedelta = 2 * pi / 32
+num_buckets = 128
+huedelta = 300 / num_buckets
+analysis_width = 2048
 
-for i in range(len(buckets) - 1):
-    xbase = i * xdelta
-#    color1 = (1, 1, 1)
-#    color2 = (1, 1, 1)
-    color1 = hsv_to_rgb(i * huedelta, 0.7, 0.7)
-    color2 = hsv_to_rgb((i + 1) * huedelta, 0.7, 0.7)
-    verts += (xbase         , 0                   , 0); colors += color1;
-    verts += (xbase + xdelta, 600 * buckets[i + 1], 0); colors += color2;
-    verts += (xbase         , 600 * buckets[i    ], 0); colors += color1;
-    verts += (xbase         , 0                   , 0); colors += color1;
-    verts += (xbase + xdelta, 0                   , 0); colors += color2;
-    verts += (xbase + xdelta, 600 * buckets[i + 1], 0); colors += color2;
+def build_colors():
+    colors = []
+    for i in range(num_buckets - 1):
+        color1 = hsv_to_rgb(i * huedelta, 0.7, 0.7)
+        color2 = hsv_to_rgb((i + 1) * huedelta, 0.7, 0.7)
+        colors += color1;
+        colors += color2;
+        colors += color1;
+        colors += color1;
+        colors += color2;
+        colors += color2;
+    return colors
+
+def build_verts(seg):
+#    freqs = np.abs(fft(seg))
+#    freqs = freqs[0:len(freqs)//2]
+    freqs = fft(seg).real
+    freqs = np.abs(freqs[0:analysis_width // 16])
+    verts = []
+    # bucketify things
+    buckets = []
+    bucket_width = int(len(freqs) / num_buckets)
+    if bucket_width <= 1:
+        buckets = freqs
+    else:
+        for i in range(len(freqs) // bucket_width):
+            buckets.append(sum(freqs[i * bucket_width : (i + 1) * bucket_width]))
+
+    bucket_max = max(np.abs(buckets))
+
+    buckets = [x / bucket_max for x in buckets]
+    xdelta = 800 / len(buckets)
+    height = 300
+    for i in range(len(buckets) - 1):
+        xbase = i * xdelta
+        verts += (xbase         , 0                      , 0)
+        verts += (xbase + xdelta, height * buckets[i + 1], 0)
+        verts += (xbase         , height * buckets[i    ], 0)
+        verts += (xbase         , 0                      , 0)
+        verts += (xbase + xdelta, 0                      , 0)
+        verts += (xbase + xdelta, height * buckets[i + 1], 0)
+    return VertexAttribute(verts)
 
 def resize(w, h):
     glLoadIdentity()
-    glTranslate(-1, -1, 0)
+    glTranslate(-1, 0, 0)
     height_scale = w / h / 400
     glScale(2 / 800, height_scale, 1)
     glViewport(0, 0, w, h)
 
 def display():
+    global frame
+    global start_time
+    frame += 1
+    tick = time()
+    if tick - start_time > audio_length:
+        tick = start_time
+    index = int(len(full_audio) * (tick - start_time) / audio_length)
+    if index == 0:
+        print('song start')
+    verts = build_verts(full_audio[index : index + analysis_width])
+    vbo.replace_data(verts, colors=colors)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     shader.bind()
     try:
@@ -61,9 +92,12 @@ def display():
       shader.unbind()
 
 create_window('plot', display=display, resize=resize)
-
-vbo = VertexBufferObject(VertexAttribute(verts), colors=VertexAttribute(colors))
+colors = VertexAttribute(build_colors())
+vbo = VertexBufferObject(build_verts(full_audio[0:analysis_width]), colors=colors)
 shader = ShaderProgram('shaders/basic.vert', 'shaders/basic.frag')
 glClearColor(0, 0, 0, 0)
+frame = 0
+
+start_time = time()
 
 main_loop()
